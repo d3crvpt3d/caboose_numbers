bab29a7f1238f075d15e34b055e0c8e285155fd8
d3crvpt3d <st4r.sprayer@gmail.com>
2024-06-20 09:16:25 +0200

    +iterate over all primes to test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::env::args;
-use primes;
+use std::{env::args, process::exit};
+use primes::{self, PrimeSet};
 use rayon::prelude::*;
 //use pbr::{self, MultiBar}; //ty https://github.com/a8m/pb/tree/master
 
@@ -9,40 +9,44 @@ fn main(){
 
   let end = usize::from_str_radix(&args[1], 10).unwrap_or(100);
 
-  //let mb = MultiBar::new();
-  //mb.println(&format!("Caboose from {} to {}", start,end));
+  //create prime array for iteration
+  let mut pset = primes::Sieve::new();
 
-  //let mut b1 = mb.create_bar(start);
-  //let mut b2 = mb.create_bar(end-start);
+  let last_p = pset.get(end);
 
-  let mut part_vec = vec![0usize; end];//create buffer for threads to write
+  let mut part_vec = vec![0u64; last_p as usize+1];//create buffer for threads to write
 
   eprintln!("Running on {:?} threads", rayon::current_num_threads());
 
+  eprintln!("Testing from 0 to {}", last_p);
 
-  //fill test buffer
+  //fill part vector from 0 to the last prime
   part_vec.par_iter_mut().enumerate().for_each(|(idx, x)| {
-    *x = idx * idx - idx;
+    *x = (idx * idx - idx) as u64;
   });
 
-
   //check if number is caboose number
-  part_vec.par_iter().enumerate().for_each(|(idx, _)| {
+  pset.iter().for_each(|prime| {
+
+    if prime as usize >= part_vec.len(){
+      eprintln!("Done");
+      exit(0);
+    }
     
-    let mut c = 0;
+    let mut c: u64 = 0;
 
-    while c <= idx {
+    while c <= prime{
 
-      if !primes::is_prime((idx + part_vec[c]) as u64){
-        if c == idx{
-          println!("{} is a caboose number", idx);
+      if !primes::is_prime(prime + part_vec[c as usize]){
+        if c == prime{
+          println!("{} is a caboose number", prime);
         }
         break;
       }
 
       c += 1;
     }
-    
+
   });
 
 }
